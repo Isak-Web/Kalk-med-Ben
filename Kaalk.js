b96adad47fc2e006007018e2541cea20de704b75
Isak-Web <70743274+Isak-Web@users.noreply.github.com>
2021-04-08 16:38:45 +0200

    Update Kaalk.js

--- Kaalk.js
+++ Kaalk.js
@@ -1,9 +1,77 @@
 let currentnumber ='0';
+let operations = [];
+let numbers = [];
+let operationMode = false;
 
 function numberpress(num){
     currentnumber ==='0' ? currentnumber = num : currentnumber+= num;
     updateDisplay(currentnumber);
+    operationMode = true;
 }
+
+function calcualte(){
+    let total = '0';
+    for (let i=0; i< numbers.length; i++ ){
+        if(i === 0 ){
+            total = Number.parseFloat(numbers[i]);
+        }else {
+            total = operate(total, Number.parseFloat(numbers[i]), operations[i-1]);
+        }
+    } return `${total}`;
+}
+
+
+function operate(val1, val2, op){
+switch (op) {
+    case 'add': 
+        return val1 + val2;
+         
+    case 'subtract':
+        return val1 - val2;
+
+    case 'multiply':
+        return val1 * val2;
+        
+    case 'divide':
+        return val1 / val2;
+    
+    default:
+        return '0';
+
+    }
+
+}
+
+function reset(num = '0') {
+    numbers = [];
+    operations =[];
+    currentnumber = num;
+}
+
+
+
+
+function operatorPress(op){
+    if(!operationMode) {
+        return;
+    }
+
+    operations.push(op);
+    numbers.push(currentnumber);
+
+    if(op ==='equal'){
+        currentnumber = calcualte()
+        updateDisplay(currentnumber);
+        reset(currentnumber);
+    } else{
+        currentnumber = '0';
+        updateDisplay();
+        operationMode = false;
+    }
+
+}
+
+
 
 function updateDisplay(num = '0')
 {
@@ -16,11 +84,12 @@ function updateDisplay(num = '0')
 function buttonpress(info){
     if(info === 'dot') 
     {
-        currentnumber = currentnumber + '.';
+        currentnumber = currentnumber + '.'; //if current nymber has more then one dot
         updateDisplay(currentnumber);
     }
     else if(info=== 'clear')
     {
+        reset();
         updateDisplay();
     }
     else if(info ==='percent')
@@ -36,7 +105,10 @@ function buttonpress(info){
     else if(!isNaN(Number.parseInt(info))) {
         numberpress(info);
     }
-    else{}
+    else{
+        operatorPress(info);
+    }
+        
 
 }
 
